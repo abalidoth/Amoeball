08b3b2e37396d1ad6b38aee06ebf8fd83d005d7d
Gavin Daphne King <gdking01@gmail.com>
2025-08-17 17:24:21 -0700

    Fixed disconnections

--- UI/GameUI.gd
+++ UI/GameUI.gd
@@ -168,8 +168,12 @@ func _on_replay_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	if game_is_over:
+		if $/root/MultiplayerConnector.server_active:
+			$/root/MultiplayerConnector.disconnect_server()
 		get_tree().change_scene_to_file("res://UI/main_menu.tscn")
 
 
 func _on_back_button_pressed():
+	if $/root/MultiplayerConnector.server_active:
+		$/root/MultiplayerConnector.disconnect_server()
 	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
--- multiplayer_connector.gd
+++ multiplayer_connector.gd
@@ -2,6 +2,7 @@ extends Node
 
 var ConnectedRoom: MatchaRoom
 var GamePeer: MatchaPeer
+var server_active: bool = false
 
 signal server_started(room_id:String)
 signal client_has_joined()
@@ -13,19 +14,27 @@ func initiate_server() -> void:
 	ConnectedRoom.peer_joined.connect(client_joined)
 	ConnectedRoom.peer_left.connect(client_left)
 	server_started.emit(ConnectedRoom.room_id)
+	server_active = true
 	
 func connect_to_server(server_id:String):
 	ConnectedRoom = MatchaRoom.create_client_room(server_id)
 	ConnectedRoom.peer_joined.connect(client_joined)
 	ConnectedRoom.peer_left.connect(client_left)
+	server_active = true
 	
 func client_joined(client_id:int, peer:MatchaPeer) -> void:
 	GamePeer = peer
 	peer.on_event("move",hear_move)
 	client_has_joined.emit()
 	
+func disconnect_server():
+	if server_active:
+		for peer in ConnectedRoom.get_peers():
+			ConnectedRoom.remove_peer(peer)
+	
 func client_left(client_id:int, peer:MatchaPeer) -> void:
 	client_has_left.emit()
+	server_active = false
 
 func send_move(move:String) -> void:
 	GamePeer.send_event("move",[move])
